e5d2c174cd812446a891309af04fc4eae507d5d7
IcePrince7 <sekarsakthi73@outlook.com>
2024-05-18 17:49:11 +0530

    C3: Unknown Exception

--- src/prof.js
+++ src/prof.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./prof.css";
 
-function Profile(params) {
+function Profile({params}) {
+  const [enlarged, setEnlarged] = useState(false);
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === "Escape") {
+        setEnlarged(false);
+        params(false)
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [params]);
+
+  const toggleEnlarged = () => {
+    params(!enlarged)
+    setEnlarged(!enlarged);
+  };
+
   return (
-    <div className="profile">
-      <img className="Myprof" src="myPhoto.png" alt="prof" />
+    <div className= {!enlarged ? "profile": "after enprof"} onClick={toggleEnlarged}>
+      <img
+        className={enlarged ? "Myprof enlarged" : "Myprof"}
+        src="myPhoto.png"
+        alt="prof"
+      />
     </div>
   );
 }
