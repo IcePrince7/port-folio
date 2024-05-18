d67a41cd5dfa8a6dd113d451090123760948ef86
IcePrince7 <sekarsakthi73@outlook.com>
2024-05-18 17:58:08 +0530

    C5: Commiting for mismatch

--- src/App.js
+++ src/App.js
@@ -8,56 +8,69 @@ import Languages from "./languages";
 import Project from "./project";
 import Aboutpage from "./aboutpage";
 
-
-
 function App() {
-  const [today, setDate] = React.useState(new Date());
+  const [enlargeapp, setEnlargeapp] = React.useState(false);
   return (
     <>
-      <Router>
-        <Profile style={{ alignItems: "center" }}></Profile>
-        <Cont_maker
-          style={{ justifyContent: "center" }}
-          toCreate="Sakthivel G"
-          tag="h1"
-        ></Cont_maker>
-        <div className="gridCont">
-          <Cont_maker toCreate="Sekarsakthi73@gmail.com" tag="p"></Cont_maker>
-          <Cont_maker toCreate="linkedin/Sakthi73" tag="p"></Cont_maker>
-          <Cont_maker
-            toCreate="1/8, Vennanthur, Minnakal (po), Namakkal (DT), Tamil Nadu"
-            tag="p"
-          ></Cont_maker>
-        </div>
-        <div className="TwoHalves">
-          <div className="NavBar">
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/whoami" activeClassName="active">Who am I</Link>
-                </li>
-                <li>
-                  <Link to="/languages" activeClassName="active">My expertise</Link>
-                </li>
-                <li>
-                  <Link to="/proj" activeClassName="active">Projects</Link>
-                </li>
-                <li>
-                  <Link to="/" activeClassName="active">About </Link>
-                </li>
-              </ul>
-            </nav>
+      <div className={enlargeapp ? "mainClass" : "mainClassNarrow"}>
+        <Router>
+          <Profile params={setEnlargeapp}></Profile>
+          <div className={enlargeapp ? "page" : ""}>
+            <Cont_maker
+              style={{ justifyContent: "center" }}
+              toCreate="Sakthivel G"
+              tag="h1"
+            ></Cont_maker>
+            <div className="gridCont">
+              <Cont_maker
+                toCreate="Sekarsakthi73@gmail.com"
+                tag="p"
+              ></Cont_maker>
+              <Cont_maker toCreate="linkedin/Sakthi73" tag="p"></Cont_maker>
+              <Cont_maker
+                toCreate="1/8, Vennanthur, Minnakal (po), Namakkal (DT), Tamil Nadu"
+                tag="p"
+              ></Cont_maker>
+            </div>
+            <div className="TwoHalves">
+              <div className="NavBar">
+                <nav>
+                  <ul>
+                    <li>
+                      <Link to="/whoami" activeClassName="active">
+                        Who am I
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/languages" activeClassName="active">
+                        My expertise
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/proj" activeClassName="active">
+                        Projects
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/" activeClassName="active">
+                        About{" "}
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              <div className="ComponentArena">
+                <Routes>
+                  <Route path="/whoami" element={<WhoAmI />} />
+                  <Route path="/languages" element={<Languages />} />
+                  <Route path="/proj" element={<Project />} />
+                  <Route path="/" element={<Aboutpage />} />
+                </Routes>
+              </div>
+            </div>
           </div>
-          <div className="ComponentArena">
-            <Routes>
-              <Route path="/whoami" element={<WhoAmI />} />
-              <Route path="/languages" element={<Languages />} />
-              <Route path="/proj" element={<Project />} />
-              <Route path="/" element={<Aboutpage />} />
-            </Routes>
-          </div>
-        </div>
-      </Router>
+        </Router>
+      </div>
     </>
   );
 }
